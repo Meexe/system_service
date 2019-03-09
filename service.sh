56a1f0a3eda6dbe07c59b3d9e5cbb381f500efbd
Maxim Pak <maxpak99@gmail.com>
2019-03-09 20:07:49 +0000

    fix: useless output

--- service.sh
+++ service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 )";
-function check_ssh { 
-	OUTPUT="$( diff <(echo "$TMP") <(netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8) | grep ">" )";
+TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
+function check_ssh {
+	OUTPUT="$( diff <(echo "$TMP") <(netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" ) | grep ">" )";
 	[[ ! -z "$OUTPUT" ]] && awk '{ print $0, "logged in via SSH" }' <(echo "$OUTPUT") | wall;
-	TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 )";
+	TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
 }
 
 trap 'check_ssh' USR1;
