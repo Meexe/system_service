dc31e5ce934fab81d2977f7dbdebbdaeedfcafcd
Maxim Pak <maxpak99@gmail.com>
2019-03-09 20:13:46 +0000

    fix: useless output - Pak Maxim

--- service.sh
+++ service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TMP="$( ps auxwww | grep sshd: | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f12 )";
-function check_ssh { 
-	OUTPUT="$( ps auxwww | grep sshd: | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f12 )";
+TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
+function check_ssh {
+	OUTPUT="$( diff <(echo "$TMP") <(netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" ) | grep ">" )";
 	[[ ! -z "$OUTPUT" ]] && awk '{ print $0, "logged in via SSH" }' <(echo "$OUTPUT") | wall;
-	TMP="$( ps auxwww | grep sshd: | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f12 )";
+	TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
 }
 
 trap 'check_ssh' USR1;
