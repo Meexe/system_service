d1299bcdd65ef8e5e0e9213387735fe232c9c007
ChesnovAE <aechesnov@yandex.ru>
2019-05-19 17:32:32 -0400

    add: rpm package without selinux policy - Anton Chesnov

---
+++ rpmbuild/BUILD/ssh-monitoring-0.1/ssh-monitoring.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
+function check_ssh {
+	OUTPUT="$( diff <(echo "$TMP") <(netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" ) | grep ">" )";
+	TMP="$( netstat -atp | grep 'ESTABLISHED.*sshd' | tr -s ' ' ' ' | cut -d' ' -f8 | grep -v "\[" )";
+}
+
+trap ' check_ssh; [[ ! -z "$OUTPUT" ]] && awk "{ print $0, "logged in via SSH" }" <(echo "$OUTPUT"); logger SIGUSR1 recieved "$OUTPUT" ' USR1;
+
+while true; do
+	check_ssh
+	[[ ! -z "$OUTPUT" ]] && awk '{ print $0, "logged in via SSH" }' <(echo "$OUTPUT") | wall
+	[[ ! -z "$OUTPUT" ]] && logger "$OUTPUT"
+	sleep 5 &
+	wait
+done;
+
+exit 0
+
