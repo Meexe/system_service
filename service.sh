8dd0c1f71245ce88e3aaba7bedbd699436aeb49b
Maxim Pak <maxpak99@gmail.com>
2019-05-19 19:14:36 +0000

    add: logging start/stop/restart - Pak Maxim

--- service.sh
+++ service.sh
@@ -7,11 +7,16 @@ function check_ssh {
 }
 
 trap ' check_ssh; [[ ! -z "$OUTPUT" ]] && awk "{ print $0, "logged in via SSH" }" <(echo "$OUTPUT"); logger SIGUSR1 recieved "$OUTPUT" ' USR1;
+trap ' logger SSH-monitoring service stopped && exit 0' SIGTERM;
 
-while sleep 5; do
+logger SSH-Monitoring service started;
+
+while true; do
 	check_ssh
 	[[ ! -z "$OUTPUT" ]] && awk '{ print $0, "logged in via SSH" }' <(echo "$OUTPUT") | wall
 	[[ ! -z "$OUTPUT" ]] && logger "$OUTPUT"
+	sleep 5 &
+	wait
 done;
 
 exit 0
