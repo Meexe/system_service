39a9876468c7288b490c348720ae767a86191bb7
Maxim Pak <maxpak99@gmail.com>
2019-05-20 04:22:12 +0000

    fix: unnecessary output - Pak Maxim

--- ssh-monitoring-lib.sh
+++ ssh-monitoring-lib.sh
@@ -2,7 +2,7 @@
 
 TMP="$( ps aux | grep ssh | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f1 )";
 function check_ssh {
-	OUTPUT="$( diff <(echo "$TMP") <(ps aux | grep ssh | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f1 ) | grep ">" )";
+	OUTPUT="$( diff <(echo "$TMP") <(ps aux | grep ssh | grep @ | grep -v grep | grep -v cut | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f1 ) | grep ">" )";
 	TMP="$( ps aux | grep ssh | grep @ | tr -s ' ' ' ' | cut -d' ' -f12 | cut -d'@' -f1 )";
 }
 
